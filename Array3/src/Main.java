df37ee86cd51a815356029cb865f094d2dc2cc38
marcoferraro49 <160497711+marcoferraro49@users.noreply.github.com>
2024-02-22 13:41:43 +0100

    esercizio array3

---
+++ Array3/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] values = new int[]{1, 1, 3, 3, 5, 5};
+        System.out.println(Arrays.toString(check(values)));
+    }
+
+    public static int[] check(int[] array) {
+        int[] temp = new int[array.length];
+        int j = 0;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] != array[i + 1]) {
+                temp[j++] = array[i];
+            } else {
+                temp[j++] = -1;
+            }
+        }
+        
+        temp[j++] = array[array.length - 1];
+        return temp;
+    }
+}
