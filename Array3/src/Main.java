8d363532e7b35c7f229e09b617d7406b04af5592
marcoferraro49 <160497711+marcoferraro49@users.noreply.github.com>
2024-02-23 17:04:17 +0100

    soluzione definitiva esercizio Array3

--- Array3/src/Main.java
+++ Array3/src/Main.java
@@ -2,23 +2,22 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] values = new int[]{1, 1, 3, 3, 5, 5};
-        System.out.println(Arrays.toString(check(values)));
+        int[] numeriACaso = {1, 1, 20, -1, 3, -4, 28, 12, 1, 0, 2, -4};
+        trovaDuplicati(numeriACaso);
+        for (int i = 0; i < numeriACaso.length; i++){
+            System.out.print(numeriACaso[i] + " ");
+        }
     }
 
-    public static int[] check(int[] array) {
-        int[] temp = new int[array.length];
-        int j = 0;
-
-        for (int i = 0; i < array.length - 1; i++) {
-            if (array[i] != array[i + 1]) {
-                temp[j++] = array[i];
-            } else {
-                temp[j++] = -1;
+    public static void trovaDuplicati(int[] arrayDiInteri) {
+        for (int i = 0; i < arrayDiInteri.length - 1; i++){
+            if (arrayDiInteri[i] != -1){
+                for (int k = i+1; k < arrayDiInteri.length; k++){
+                    if (arrayDiInteri[i] == arrayDiInteri[k]){
+                        arrayDiInteri[k] = -1;
+                    }
+                }
             }
         }
-        
-        temp[j] = array[array.length - 1];
-        return temp;
     }
 }
